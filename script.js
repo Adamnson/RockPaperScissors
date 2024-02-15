de52d50102638706046d8c374f97abb65a8814e7
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-02-15 23:27:22 +0530

    display game progress in html, stop after scoring

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const game = document.querySelector('.game');
+let playerScore = 0;
+let compScore = 0;
+
 // a function to assign a computer choice 
 // depending on a random number generator
 function getComputerChoice() {
@@ -24,9 +28,10 @@ function calcWinner(playerSelection, computerSelection) {
   let comp   = computerSelection.toUpperCase();
   let winFlag = false;
   let repFlag = false;
+  let message = '';
 
-  console.log("player:", player);
-  console.log("computer:", comp);
+  // console.log("player:", player);
+  // console.log("computer:", comp);
 
   if (player === comp) {
     repFlag = true;
@@ -51,15 +56,42 @@ function calcWinner(playerSelection, computerSelection) {
   }
 
   if (!repFlag && winFlag) {
-    console.log("Winner!!");
+    // console.log("Winner!!");
+    message = " Winner!!";
+    playerScore++;
   }
   else if (!repFlag && !winFlag){
-    console.log("Oh ho! You lost!");
+    // console.log("Oh ho! You lost!");
+    message = " Oh ho! You lost!";
+    compScore++;
   }
   else {
     console.log("ROCK....PAPER....SCISSOR.....")
-    calcWinner(playerSelection, getComputerChoice());
+    message += " playing again, make a selection";
+
+    // calcWinner(playerSelection, getComputerChoice());
   }
+
+  addToHTML(player, comp, message, playerScore, compScore);
+  declareResult(playerScore, compScore);
+}
+
+function addToHTML(player, comp, message) {
+  const tempEle = document.createElement('p');
+  let content = '';
+  content += "Player's choice : " +  player ;
+  content += "</br>";
+  content += "Computer's choice :" +  comp;
+  content += "</br>";
+  content += message;
+  content += "</br>";
+  content += "Player's score : " + playerScore;
+  content += "</br>";
+  content += "Computer's score : " + compScore;
+  content += "</br>";
+
+  tempEle.innerHTML = content;
+  game.appendChild(tempEle);
 }
 
 const options = document.querySelector(".options");
@@ -69,4 +101,18 @@ options.addEventListener('click', function(event) {
   calcWinner(event.target.id, getComputerChoice());
 });
 
+function declareResult(playerScore, compScore) {
+  if(playerScore === 5 || compScore === 5) {
+    const decision = document.createElement('p');
+    let result = '';
+    (playerScore === 5) ?
+    result = 'Congratulations! You won the game' :
+    result = 'Better Luck Next Time';
+
+    decision.innerHTML = result;
+    game.appendChild(decision);
+    const btns = document.querySelectorAll('button')
+    btns.forEach((btn)=> btn.disabled = true);
+  }
+}
 // calcWinner('rock', getComputerChoice());
